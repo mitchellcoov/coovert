462dbb9dd7bb3edf1bbd595e1ef2844a3eb0ed5d
Mitchell Coovert <mitchell_coovert@hotmail.com>
2024-10-08 14:16:32 -0400

    Add the docker compose and traefik setup
    - I've added a docker compose file making it easier to run on server
    - used traefik, found it to be nicer than nginx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
     <div>
       <div className="grid grid-rows-[450px_minmax(50px,_100px)_30px_minmax(50px,_100px)] justify-center justify-items-center items-center lg:flex lg:flex-row lg:justify-between  bg-img">
         
-        <a className="row-start-2" href="http://mable.coovert.gay">
+        <a className="row-start-2" href="https://mable.coovert.gay">
           <h1 className="hover:neonText text-5xl lg:text-7xl lg:pl-40 " >mable</h1>
         </a>
         <h1 className="text-5xl lg:text-7xl self-end mb-40" >choose your coovert</h1>
-        <a className="row-start-4" href="http://mitchell.coovert.ca">
+        <a className="row-start-4" href="https://mitchell.coovert.ca">
           <h1 className="hover:neonText text-5xl lg:text-7xl lg:pr-12" >mitchell</h1>
         </a>
       </div>
